476bc9af85d70fdc431193592d0895015fa9aa3e
André Gaul <gaul@web-yard.de>
2014-09-18 14:11:05 +0800

    add basic tag selector

--- src/js/mneme.js
+++ src/js/mneme.js
@@ -2,15 +2,28 @@
 var mneme = angular.module('mneme', ['ngRoute']);
 
 // add mneme-tags element (realized via angularjs' directives)
-mneme.directive('mnemeTags', function() {
+mneme.directive('mnemeTags', function () {
   return {
     restrict: 'E',
-    templateUrl: 'templates/mneme-tags.html'
+    templateUrl: 'templates/mneme-tags.html',
+    scope: {
+      tags_get: '=modelGet',
+      tags_selected: '=modelSelected',
+    },
+    link: function(scope, element, attrs) {
+      scope.tags_selected = scope.tags_selected || [];
+      scope.add = function (tag) {
+        scope.tags_selected.push(tag);
+      };
+      scope.remove = function (tag) {
+        scope.tags_selected.splice(scope.tags_selected.indexOf(tag), 1);
+      };
+    }
   };
 });
 
 // set up routes
-mneme.config(function($routeProvider) {
+mneme.config(function ($routeProvider) {
   $routeProvider.
     when('/tags', {
       templateUrl: 'templates/tags.html'
@@ -18,4 +31,14 @@ mneme.config(function($routeProvider) {
     otherwise({
       redirectTo: '/tags'
     });
+});
+
+// set up Overview controller
+mneme.controller('OverviewCtrl', function ($scope) {
+  $scope.tags_get = function (tags_selected) {
+    var tags = ['Restaurant', 'Italian', 'Chinese', 'Vietnames', 'never been there'];
+    return tags.filter(function (el) {
+      return tags_selected.indexOf(el)===-1;
+    });
+  };
 });
