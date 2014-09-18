a4a50504f74568d91543a7ba3d5397b19a6219d8
André Gaul <gaul@web-yard.de>
2014-09-18 13:16:17 +0800

    dummy <mneme-tags> element directive

--- src/js/mneme.js
+++ src/js/mneme.js
@@ -1,2 +1,21 @@
-//Setup Angular
-var mneme = angular.module('mneme', []);
+// set up angular
+var mneme = angular.module('mneme', ['ngRoute']);
+
+// add mneme-tags element (realized via angularjs' directives)
+mneme.directive('mnemeTags', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/mneme-tags.html'
+  };
+});
+
+// set up routes
+mneme.config(function($routeProvider) {
+  $routeProvider.
+    when('/tags', {
+      templateUrl: 'templates/tags.html'
+    }).
+    otherwise({
+      redirectTo: '/tags'
+    });
+});
