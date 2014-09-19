76682229d2ee6ca79157e045c41ac780e5f92799
André Gaul <gaul@web-yard.de>
2014-09-19 13:29:33 +0800

    style tags in list

--- src/js/mneme.js
+++ src/js/mneme.js
@@ -39,6 +39,9 @@ mneme.config(function ($routeProvider) {
 // set up Overview controller
 mneme.controller('OverviewCtrl', function ($scope) {
   $scope.tagnames_selected = [];
+  $scope.contains = function (tag) {
+    return _.contains($scope.tagnames_selected, tag);
+  };
   $scope.mnemes = [
     {
       name: 'Soup populaire',
